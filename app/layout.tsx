fcac77fe5f2c4bcc2605b3e2e18e5ba99222aecb
Habte369t <habtamuayele369t@gmail.com>
2025-08-08 00:01:50 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-white text-black">{children}</body>
+    </html>
+  );
+}
+Create layout.tsx
